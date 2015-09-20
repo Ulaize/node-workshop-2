3ab2a5ab4bb746afe519046f8cd3ebe1bd5abc8f
Ulaize <ulaize@creative-tweed.com>
2015-09-20 18:18:18 +0000

    Fixed some location-server.js. Still not working

--- location-server.js
+++ location-server.js
@@ -1,4 +1,4 @@
-//Location server AIzaSyDtEGHKLUczLAayBS3ZPHMr5AgHpREqWYQ  headers.URL
+//API 6652bf154c2c8f63098f4df13f50ef9f
 
 var request = require('request');
 var prompt = require('prompt');
@@ -9,11 +9,10 @@ var requestListener = function (cReq, cRes) {
 
         var slug = cReq.url.substring(1);
         if (cReq.url !== "/favicon.ico"){
-        console.log(cReq);
+   
         var locationRquested ="https://maps.googleapis.com/maps/api/geocode/json?address=" + slug + "&key=AIzaSyDtEGHKLUczLAayBS3ZPHMr5AgHpREqWYQ";
         request(locationRquested, function(err, res, body) {
                 var googleInfo = JSON.parse(body);
-                
                 var x = googleInfo.results[0].geometry.location.lat;
 
                 var y = googleInfo.results[0].geometry.location.lng;
