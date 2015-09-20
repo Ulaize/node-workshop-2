8dbf01aa39496dcb19b370a438c3067e853891a8
Ulaize <ulaize@creative-tweed.com>
2015-09-20 16:18:18 +0000

    Not finalised cached location server

---
+++ cached-location-server.js
@@ -0,0 +1,41 @@
+//Location server AIzaSyDtEGHKLUczLAayBS3ZPHMr5AgHpREqWYQ  headers.URL
+
+var request = require('request');
+var http = require('http');
+
+var cachedRequest =[];
+var cachedLocation=[];
+
+var requestListener = function (cReq, cRes) {
+        
+        if (cReq.url !== "/favicon.ico"){
+                for (var i=0; i< cachedLocation.length; i++){
+                        var slug = cReq.url.substring(1);
+                        if(cachedRequest[i]===slug){
+                                cRes.writeHead(200);
+                                cRes.end(cachedLocation[i]+ ". This is a cached response");
+                        }
+                        else {
+                                var locationRquested ="https://maps.googleapis.com/maps/api/geocode/json?address=" + slug + "&key=AIzaSyDtEGHKLUczLAayBS3ZPHMr5AgHpREqWYQ";
+                                request(locationRquested, function(err, res, body) {
+                                        var googleInfo = JSON.parse(body);
+                                        var x = googleInfo.results[0].geometry.location.lat;
+                                        var y = googleInfo.results[0].geometry.location.lng;
+                                        cachedLocation.push("Your location is : " + x + " " +y);
+                                        
+                                        cRes.writeHead(200);
+                                        cRes.end("Your location: "+x+y + ". This is a fresh response");
+                                        
+                                });
+                                    
+                                
+
+                        }
+                }
+               
+        }
+};
+
+var server = http.createServer(requestListener);
+
+server.listen(process.env.PORT, process.env.IP);
